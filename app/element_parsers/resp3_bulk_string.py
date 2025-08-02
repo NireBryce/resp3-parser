70defc17cfd5562856415d1f0e058fe4af0d892d
Nire Bryce <nire@computernope.net>
2025-08-02 16:42:10 -0400

    bulk string minimal

--- app/element_parsers/resp3_bulk_string.py
+++ app/element_parsers/resp3_bulk_string.py
@@ -3,19 +3,26 @@ from ..util import slice_first_byte
 def parse_bulk_string(data: bytes):
     if slice_first_byte(data) != b"$":
         raise ValueError(f"Expected '$' for bulk string prefix, got {data[0]}")
-    _prefix, _string = data.split(b"$", 1)
-    _length, _string = _string.split(CRLF, 1)
+    length = data.split(CRLF)[1]
+    print(f"bulk string, length: {length}")
+
+
+# def parse_bulk_string(data: bytes):
+#     if slice_first_byte(data) != b"$":
+#         raise ValueError(f"Expected '$' for bulk string prefix, got {data[0]}")
+#     _prefix, _string = data.split(b"$", 1)
+#     _length, _string = _string.split(CRLF, 1)
     
-    # TODO: hack 
-    if len(_string.split(CRLF, 1)) > 1:
-        _string, _remaining = _string.split(CRLF, 1)
-    else:
-        _string, _remaining = _string, b""
-    # /hack
+#     # TODO: hack 
+#     if len(_string.split(CRLF, 1)) > 1:
+#         _string, _remaining = _string.split(CRLF, 1)
+#     else:
+#         _string, _remaining = _string, b""
+#     # /hack
     
     
-    if _length == b"-1": 
-        _string = None
-    else:
-        _string = _string[:int(_length)].decode()
-    return _string
+#     if _length == b"-1": 
+#         _string = None
+#     else:
+#         _string = _string[:int(_length)].decode()
+#     return _string
