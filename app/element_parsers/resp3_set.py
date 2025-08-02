aa0c1ccab1159619dde08e231af4d02e542c93e6
Nire Bryce <nire@computernope.net>
2025-08-02 16:36:20 -0400

    resp3 set brought to mvp

--- app/element_parsers/resp3_set.py
+++ app/element_parsers/resp3_set.py
@@ -5,9 +5,18 @@ def parse_set(data: bytes):
         raise ValueError(f"Expected '~' for set prefix, got {data[0]}")
     _prefix, _data = data.split(b"~", 1)
     _length, _data = _data.split(CRLF, 1)
-    _set = set()
-    for _ in range(int(_length)):
-        _element, _data = parse_element(_data)
-        _set.add(_element)
-    _remaining = _data
-    return _set, _remaining
+    
+    print(f"set, length: {_length}")
+
+
+# def parse_set(data: bytes):
+#     if slice_first_byte(data) != b"~":
+#         raise ValueError(f"Expected '~' for set prefix, got {data[0]}")
+#     _prefix, _data = data.split(b"~", 1)
+#     _length, _data = _data.split(CRLF, 1)
+#     _set = set()
+#     for _ in range(int(_length)):
+#         _element, _data = parse_element(_data)
+#         _set.add(_element)
+#     _remaining = _data
+#     return _set, _remaining
