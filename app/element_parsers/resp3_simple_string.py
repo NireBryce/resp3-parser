c11c826c3eeb73ded7a4dc4e106b4aa3cc3c954a
Nire Bryce <nire@computernope.net>
2025-08-02 16:21:53 -0400

    simple string rework start

--- app/element_parsers/resp3_simple_string.py
+++ app/element_parsers/resp3_simple_string.py
@@ -1,9 +1,25 @@
 from ..CONSTANTS import TEXT_ENCODING, CRLF
 from ..util import slice_first_byte
-def parse_simple_string(data: bytes) -> tuple[str, bytes]:
-    # +OK\r\n
+
+
+def parse_simple_string(data: bytes):
+    """ Take a RESP3 simple string representation as bytes and return the 
+        string it contains, as a string
+    
+        example: +OK\r\n
+    """
     if slice_first_byte(data) != b"+":
         raise ValueError(f"Expected '+' for simple string prefix, got {data[0]}")
-    _prefix, _str = data.split(b"+", 1)
-    _str, _remaining = _str.split(CRLF, 1)
-    return str(_str, TEXT_ENCODING), _remaining
+    return print("Simple String")
+
+
+
+
+
+# def parse_simple_string(data: bytes) -> tuple[str, bytes]:
+#     # +OK\r\n
+#     if slice_first_byte(data) != b"+":
+#         raise ValueError(f"Expected '+' for simple string prefix, got {data[0]}")
+#     _prefix, _str = data.split(b"+", 1)
+#     _str, _remaining = _str.split(CRLF, 1)
+#     return str(_str, TEXT_ENCODING), _remaining
