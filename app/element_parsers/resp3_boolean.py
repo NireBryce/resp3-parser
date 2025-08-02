280df355fdaccc71232128c23772dcb00f9afa73
Nire Bryce <nire@computernope.net>
2025-08-02 16:43:27 -0400

    resp3 bool minimal

--- app/element_parsers/resp3_boolean.py
+++ app/element_parsers/resp3_boolean.py
@@ -3,12 +3,19 @@ from ..util import slice_first_byte
 def parse_boolean(data:bytes):
         if slice_first_byte(data) != b"#":
             raise ValueError(f"Expected '#' for boolean prefix, got {data[0]}")
-        _prefix, _bool = data.split(b"#", 1)
-        _bool, _remaining = _bool.split(CRLF, 1)
-        if _bool == b"t":
-            _result = True
-        elif _bool == b"f":
-            _result = False
-        else:
-            raise ValueError
-        return _result
+        print("boolean")
+
+
+
+# def parse_boolean(data:bytes):
+#         if slice_first_byte(data) != b"#":
+#             raise ValueError(f"Expected '#' for boolean prefix, got {data[0]}")
+#         _prefix, _bool = data.split(b"#", 1)
+#         _bool, _remaining = _bool.split(CRLF, 1)
+#         if _bool == b"t":
+#             _result = True
+#         elif _bool == b"f":
+#             _result = False
+#         else:
+#             raise ValueError
+#         return _result
