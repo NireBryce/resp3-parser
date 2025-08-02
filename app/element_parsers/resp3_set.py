de487753539f0b148789f08d65d44ba68444654d
Nire Bryce <nire@computernope.net>
2025-08-02 16:37:12 -0400

    set length fixd to standardize

--- app/element_parsers/resp3_set.py
+++ app/element_parsers/resp3_set.py
@@ -3,10 +3,11 @@ from ..util import slice_first_byte
 def parse_set(data: bytes):
     if slice_first_byte(data) != b"~":
         raise ValueError(f"Expected '~' for set prefix, got {data[0]}")
-    _prefix, _data = data.split(b"~", 1)
-    _length, _data = _data.split(CRLF, 1)
+    # _prefix, _data = data.split(b"~", 1)
+    # _length, _data = _data.split(CRLF, 1)
+    length = data.split(CRLF)[1]
     
-    print(f"set, length: {_length}")
+    print(f"set, length: {length}")
 
 
 # def parse_set(data: bytes):
