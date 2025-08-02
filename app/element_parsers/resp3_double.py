6a7791102734303d839648a0099f28cf88f3b435
Nire Bryce <nire@computernope.net>
2025-08-02 16:41:39 -0400

    resp3 double minimal

--- app/element_parsers/resp3_double.py
+++ app/element_parsers/resp3_double.py
@@ -4,6 +4,14 @@ from ..util import slice_first_byte
 def parse_double(data: bytes):
     if slice_first_byte(data) != b",":
         raise ValueError(f"Expected ',' for double-precision prefix, got {data[0]}")
-    _prefix, _data = data.split(b",", 1)
-    _data, _remaining = _data.split(CRLF, 1)
-    return float(_data)
+    print("double")
+
+
+# def parse_double(data: bytes):
+#    """ it matches the float format for python, so we can just pass it on through
+#    """
+#     if slice_first_byte(data) != b",":
+#         raise ValueError(f"Expected ',' for double-precision prefix, got {data[0]}")
+#     _prefix, _data = data.split(b",", 1)
+#     _data, _remaining = _data.split(CRLF, 1)
+#     return float(_data)
