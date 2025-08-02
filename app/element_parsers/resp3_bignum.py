0163736983812d79048cd080008d71d4ffcbe47e
Nire Bryce <nire@computernope.net>
2025-08-02 16:44:00 -0400

    resp3 bignum minimal

--- app/element_parsers/resp3_bignum.py
+++ app/element_parsers/resp3_bignum.py
@@ -3,7 +3,13 @@ from ..util import slice_first_byte
 def parse_bignum(data: bytes):
     if slice_first_byte(data) != b"(":
         raise ValueError(f"Expected '(' for bignum prefix, got {data[0]}")
+    print("bignum")
 
-    _prefix, _data = data.split(b"(", 1)
-    _data, _remaining = _data.split(CRLF, 1)
-    return int(_data)
+
+# def parse_bignum(data: bytes):
+#     if slice_first_byte(data) != b"(":
+#         raise ValueError(f"Expected '(' for bignum prefix, got {data[0]}")
+
+#     _prefix, _data = data.split(b"(", 1)
+#     _data, _remaining = _data.split(CRLF, 1)
+#     return int(_data)
