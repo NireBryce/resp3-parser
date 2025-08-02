1ac1ba2994eccdf42142c21d366cd2386c7bf76f
Nire Bryce <nire@computernope.net>
2025-08-02 16:44:58 -0400

    resp3 attr minimal

--- app/element_parsers/resp3_attribute.py
+++ app/element_parsers/resp3_attribute.py
@@ -1,28 +1,36 @@
 from ..util import slice_first_byte
+from ..CONSTANTS import CRLF
 def parse_attribute(data: bytes):
-    class RESP3Attribute:
-        def __init__(self, attribs: dict, reply):
-            self.attribs = attribs # { key: { key : value, key2: value2 } }
-            self.reply = reply
-        def __repr__(self):
-            return f"RESP3Attribute({self.attribs}, {self.reply})"
-        
-    
     if slice_first_byte(data) != b"|":
         raise ValueError(f"Expected '|' for attribute prefix, got {data[0]}")
+    length = data.split(CRLF)[1]
+    print(f"attribute, length: {length}")
 
-    _prefix, _data = data.split(b"|", 1)
-    _length, _data = _data.split(CRLF, 1)
-    _attr_name = ""
-    _attr_map = {}
-    for _ in range(int(_length)):
-        _attr_name, _data = RESP3.parse_element(_data)
-        _attr_map[_attr_name], _data = RESP3.parse_element(_data)
+
+# def parse_attribute(data: bytes):
+#     class RESP3Attribute:
+#         def __init__(self, attribs: dict, reply):
+#             self.attribs = attribs # { key: { key : value, key2: value2 } }
+#             self.reply = reply
+#         def __repr__(self):
+#             return f"RESP3Attribute({self.attribs}, {self.reply})"
+        
+    
+#     if slice_first_byte(data) != b"|":
+#         raise ValueError(f"Expected '|' for attribute prefix, got {data[0]}")
+
+#     _prefix, _data = data.split(b"|", 1)
+#     _length, _data = _data.split(CRLF, 1)
+#     _attr_name = ""
+#     _attr_map = {}
+#     for _ in range(int(_length)):
+#         _attr_name, _data = RESP3.parse_element(_data)
+#         _attr_map[_attr_name], _data = RESP3.parse_element(_data)
         
         
-    _result, _data = RESP3.parse_element(_data)
+#     _result, _data = RESP3.parse_element(_data)
     
-    attrs = RESP3Attribute(_attr_map, _result)
-    _remaining = _data
+#     attrs = RESP3Attribute(_attr_map, _result)
+#     _remaining = _data
     
-    return attrs, _remaining
+#     return attrs, _remaining
