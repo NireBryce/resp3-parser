8f865577777df06ef3b355e02055561df5ba8f72
Nire Bryce <nire@computernope.net>
2025-08-02 16:42:54 -0400

    resp3 bulk error minimal

--- app/element_parsers/resp3_bulk_error.py
+++ app/element_parsers/resp3_bulk_error.py
@@ -3,9 +3,16 @@ from ..util import slice_first_byte
 def parse_bulk_error(data: bytes):
     if slice_first_byte(data) != b"!":
         raise ValueError(f"Expected '!' for bulk error prefix, got {data[0]}")
+    length = data.split(CRLF)[1]
+    print(f"bulk error, length: {length}")
 
-    _prefix, _data = data.split(b"!", 1)
-    _length, _data = _data.split(CRLF, 1)
-    _data, _remaining = _data.split(CRLF, 1)
-    _error = _data[:int(_length)]
-    return str(_error, TEXT_ENCODING)
+
+# def parse_bulk_error(data: bytes):
+#     if slice_first_byte(data) != b"!":
+#         raise ValueError(f"Expected '!' for bulk error prefix, got {data[0]}")
+
+#     _prefix, _data = data.split(b"!", 1)
+#     _length, _data = _data.split(CRLF, 1)
+#     _data, _remaining = _data.split(CRLF, 1)
+#     _error = _data[:int(_length)]
+#     return str(_error, TEXT_ENCODING)
