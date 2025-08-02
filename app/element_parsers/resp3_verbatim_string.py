04512c56509a08ab3d919d506119e09a597f2535
Nire Bryce <nire@computernope.net>
2025-08-02 16:34:59 -0400

    verbatim string returned to mvp

--- app/element_parsers/resp3_verbatim_string.py
+++ app/element_parsers/resp3_verbatim_string.py
@@ -1,20 +1,30 @@
 from ..util import slice_first_byte
+from ..CONSTANTS import CRLF
+
 def parse_verbatim_string(data: bytes):
     if slice_first_byte(data) != b"=":
         raise ValueError(f"Expected '=' for verbatim string prefix, got {data[0]}")
+    length = data.split(CRLF)[1]
+    print(f"verbatim string, length: {length}")
 
-    _prefix, _data = data.split(b"=", 1)
-    _length, _data = _data.split(CRLF, 1)
+
+
+# def parse_verbatim_string(data: bytes):
+#     if slice_first_byte(data) != b"=":
+#         raise ValueError(f"Expected '=' for verbatim string prefix, got {data[0]}")
+
+#     _prefix, _data = data.split(b"=", 1)
+#     _length, _data = _data.split(CRLF, 1)
     
-    _encoding, _data = _data.split(CRLF, 1)
-    if _encoding == b"txt":
-        _encoding = "utf-8"
-    elif _encoding == b"mkd":
-        _encoding = "utf-8"
-    else: 
-        # underspecified in spec
-        _encoding = "bytes"
+#     _encoding, _data = _data.split(CRLF, 1)
+#     if _encoding == b"txt":
+#         _encoding = "utf-8"
+#     elif _encoding == b"mkd":
+#         _encoding = "utf-8"
+#     else: 
+#         # underspecified in spec
+#         _encoding = "bytes"
         
-    _data, _remaining = _data.split(CRLF, 1)
-    _string = _data[:int(_length)]
-    return str(_string, encoding=_encoding), _remaining
+#     _data, _remaining = _data.split(CRLF, 1)
+#     _string = _data[:int(_length)]
+#     return str(_string, encoding=_encoding), _remaining
