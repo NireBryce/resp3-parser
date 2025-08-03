b3b363962275f96db38c87bc7f507c061cd3e27d
Nire Bryce <nire@computernope.net>
2025-08-02 21:33:16 -0400

    resp3 array minimum

--- app/element_parsers/resp3_array.py
+++ app/element_parsers/resp3_array.py
@@ -1,31 +1,39 @@
 from ..util import slice_first_byte
-from ..CONSTANTS import TYPES
+from ..CONSTANTS import TYPES, CRLF
 def parse_array(data: bytes):
     if slice_first_byte(data) != b'*':
         raise ValueError(f"Expected {b'*'} for array prefix, got {data[0]}")
-    _prefix, _data = data.split(b"*", 1)
-    _length, _data = _data.split(b"\r\n", 1)
-    _array = [] 
-    _array_member = []
-    if int(_length) == -1: 
-        return [None], _data
-    elif int(_length) == 0:
-        return [], _data
-    for i in range(int(_length)+1): 
-        # TODO: this needs to be redone because it doesn't work pseudo-recursively
-        # instead it just adds the first element of each loop
-        if _data and slice_first_byte(_data) in TYPES:
-            element, _data = parse_element(_data)
-            print(f"{element=}")
-            _array_member.append(element) 
-    # # TODO: also a hack
-    # if int(_length) > 1:
-    #     _array = _array.append([member for member in _array_member])
-    # elif int(_length) == 1: 
+    length = data.split(CRLF)[1]
+    print(f"array, length: {length}")
+
+
+
+# def parse_array(data: bytes):
+#     if slice_first_byte(data) != b'*':
+#         raise ValueError(f"Expected {b'*'} for array prefix, got {data[0]}")
+#     _prefix, _data = data.split(b"*", 1)
+#     _length, _data = _data.split(b"\r\n", 1)
+#     _array = [] 
+#     _array_member = []
+#     if int(_length) == -1: 
+#         return [None], _data
+#     elif int(_length) == 0:
+#         return [], _data
+#     for i in range(int(_length)+1): 
+#         # TODO: this needs to be redone because it doesn't work pseudo-recursively
+#         # instead it just adds the first element of each loop
+#         if _data and slice_first_byte(_data) in TYPES:
+#             element, _data = parse_element(_data)
+#             print(f"{element=}")
+#             _array_member.append(element) 
+#     # # TODO: also a hack
+#     # if int(_length) > 1:
+#     #     _array = _array.append([member for member in _array_member])
+#     # elif int(_length) == 1: 
     
-    # TODO: append to array such that you only add extra lists if there are extra lists.  IE, why is this not recursing
-    # it should be [x, y] or [[a,b], [x,y]] if given two lists, it needs to nest the lists.
+#     # TODO: append to array such that you only add extra lists if there are extra lists.  IE, why is this not recursing
+#     # it should be [x, y] or [[a,b], [x,y]] if given two lists, it needs to nest the lists.
     
-    _remaining = _data
-    print(_array, _remaining)
-    return _array, _remaining
+#     _remaining = _data
+#     print(_array, _remaining)
+#     return _array, _remaining
